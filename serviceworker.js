8cfcccc6a44e79b557637e74707efeacc44e54b7
Bernard Mendes <bernardtm.dev@gmail.com>
2020-04-16 18:00:51 -0300

    service worker test

--- serviceworker.js
+++ serviceworker.js
@@ -1,13 +1,49 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker
-      .register('/serviceworker.js', { scope: '/' })
-      .then(() => {
-          console.info('Developer for Life Service Worker Registered');
-      }, err => console.error("Developer for Life Service Worker registration failed: ", err));
+const BASE_CACHE_FILES = [
+  '/',
+  '/js/jquery-3.3.1.min.js',
+  '/404.html',
+  '/offline',
+  '/css/medium.css',
+  '/css/bootstrap.min.css',
+  '/css/additional.css',
+  '/css/custom.css',
+  '/manifest.json',
+  '/images/logo.png',
+  '/images/jumbotron.jpg',
+  '/js/mediumish.js',
+  '/blog',
+  '/blog/2017-08-17_upcoming-talks/',
+  '/static/about',
+  '/privacy'
+];
 
-  navigator.serviceWorker
-      .ready
-      .then(() => {
-          console.info('Developer for Life Service Worker Ready');
-      });
-}
+const OFFLINE_CACHE_FILES = [
+  '/offline/'
+];
+
+const NOT_FOUND_CACHE_FILES = [
+  '/404.html'
+];
+
+const OFFLINE_PAGE = '/offline/';
+const NOT_FOUND_PAGE = '/404.html';
+
+const CACHE_VERSIONS = {
+  assets: 'assets-v' + CACHE_VERSION,
+  content: 'content-v' + CACHE_VERSION,
+  offline: 'offline-v' + CACHE_VERSION,
+  notFound: '404-v' + CACHE_VERSION,
+};
+
+// Define MAX_TTL's in SECONDS for specific file extensions
+const MAX_TTL = {
+  '/': 3600,
+  html: 43200,
+  json: 43200,
+  js: 86400,
+  css: 86400,
+};
+
+const CACHE_BLACKLIST = [
+  (str) => !str.startsWith('https://blog.jeremylikness.com')
+];
